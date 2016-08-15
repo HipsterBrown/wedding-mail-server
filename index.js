78043083c4389314f9d98eb04140790eb912d325
HipsterBrown <headhipster@hipsterbrown.com>
2016-08-14 20:36:45 -0400

    feat: initial build

---
+++ index.js
@@ -0,0 +1,89 @@
+// native modules
+var http = require('http');
+var querystring = require('querystring');
+var url = require('url');
+
+// npm modules
+var SparkPost = require('sparkpost');
+var client = new SparkPost(process.env.API_KEY);
+
+var port = process.env.PORT || 8080;
+
+// server setup
+var server = http.createServer((request, response) => {
+  var path = url.parse(request.url, true).pathname;
+
+  switch (path) {
+    case '/mailer':
+      sendMail(request, response);
+      break;
+    default:
+      returnNotFound(request, response);
+      break;
+  }
+});
+
+server.listen(port, (error) => {
+  if (error) {
+    console.log('An error starting the server.');
+    throw new Error(error);
+  } else {
+    console.log(`The server has started on port ${port}.`);
+  }
+});
+
+// route handlers
+function sendMail(request, response) {
+  var data = '';
+
+  request.on('data', (chunk) => { data += chunk.toString() });
+
+  request.on('end', () => {
+    data = JSON.parse(data);
+
+    client.transmissions.send({
+      transmissionBody: {
+        content: {
+          from: data.from,
+          subject: data.subject,
+          html: `
+          <html>
+            <body>
+              <h1>Contact from BetterToget-Hehr</h1>
+              <p>${data.message}</p>
+            </body>
+          </html>
+          `
+        },
+        recipients: [
+          {address: 'leah2010@gmail.com'}
+        ]
+      }
+    }, (error, mailResponse) => {
+      if (error) {
+        returnServerError(response, error);
+      } else {
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.end(JSON.stringify({
+          "message": 'Mailer successful!',
+          "response": mailResponse
+        }));
+      }
+    });
+  });
+}
+
+function returnServerError(response, error) {
+  response.writeHead(500, {"Content-Type": "application/json"});
+  response.end(JSON.stringify({
+    "error": error,
+    "message": 'Sorry, something went wrong here.'
+  }));
+}
+
+function returnNotFound(request, response) {
+  response.writeHead(404, {"Content-Type": "application/json"});
+  response.end(JSON.stringify({
+    "message": 'Route not found. Please check your request and try again.'
+  }));
+}
